5a51259202e27a7960cdaf7e32f9fd4b3ffc8cb4
Aixiang Jiang <ajiangsfu@bccrc.ca>
2023-11-30 17:05:14 -0800

    confused, did I push already?

--- R/confirmVars.R
+++ R/confirmVars.R
@@ -109,7 +109,6 @@ confirmVars = function(data = NULL, standardization = FALSE, columnWise = TRUE, 
   aplot = paste("allMarks",outplot, sep="_")
   gplot = forestmodel::forest_model(xx, format_options = forestmodel::forest_model_format_options(text_size= 4, point_size = 4)) +
     ggplot2::scale_y_continuous(labels = scales::number_format(accuracy = 0.0001)) +
-    ggplot2::scale_y_continuous(labels = scales::number_format(accuracy = 0.0001)) +
     ggplot2::theme(axis.text.x = ggplot2::element_text(size=4))
   ggpubr::ggexport(gplot, filename = aplot, height = ceiling(kk/2), width = 7)
   allplot = c(allplot, list(gplot))
